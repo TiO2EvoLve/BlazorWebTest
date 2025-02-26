74743b59d98f437febd1e220df4bff758ff708ab
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2025-02-26 09:22:17 +0800

    2025/02/26

--- BlazorWebASA/obj/Debug/net8.0/BlazorWebASA.AssemblyInfo.cs
+++ BlazorWebASA/obj/Debug/net8.0/BlazorWebASA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebASA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6f82962a7a56731a3e9ee1dbb5762cb0497035")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebASA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebASA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
