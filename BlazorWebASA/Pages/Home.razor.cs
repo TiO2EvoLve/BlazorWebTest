e6c0214cd7344b86564209c0f38902e4251ddbc5
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2025-04-01 09:25:16 +0800

    20250401

---
+++ BlazorWebASA/Pages/Home.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebASA.Pages;
+
+public class Home_razor : ComponentBase
+{
+    public TimeSpan currentTime;
+
+    protected override void OnInitialized()
+    {
+        currentTime = GetTime();
+        new Timer(UpdateTime, null, 0, 1000);
+    }
+    private void UpdateTime(object state)
+    {
+        currentTime = GetTime();
+        InvokeAsync(StateHasChanged);
+    }
+
+    private TimeSpan GetTime()
+    {
+        DateTime StartTime = new DateTime(2025,3,27,0,0,0);
+        DateTime NowTime = DateTime.Now;
+        TimeSpan timeSpan = NowTime - StartTime;
+        return timeSpan;
+    }
+}
--- BlazorWebASA/obj/Debug/net8.0/BlazorWebASA.AssemblyInfo.cs
+++ BlazorWebASA/obj/Debug/net8.0/BlazorWebASA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebASA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5685d527058f053d4f8a99323d108ddc041a0491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d2091e418dee712573c21529b2259f2e44dbc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebASA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebASA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
