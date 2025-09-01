2b386f5110df3a47654869b540786aeaec53f9c8
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2025-09-01 10:31:54 +0800

    20250901

---
+++ wwwroot/js/steamApi.js
@@ -0,0 +1,7 @@
+﻿// ...existing code...
+export async function getSteamUser(steamid) {
+  const response = await fetch(`/.netlify/functions/steam-proxy?steamid=${steamid}`);
+  return await response.json();
+}
+// ...existing code...
+
