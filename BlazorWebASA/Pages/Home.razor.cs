6b5233df88d171afef5c946ddc016ec85f8cfc28
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2026-06-04 11:58:24 +0800

    优化显示，替换文字

--- BlazorWebASA/Pages/Home.razor.cs
+++ BlazorWebASA/Pages/Home.razor.cs
@@ -26,9 +26,9 @@ public class Home_razor : ComponentBase
         new() { Name = "UE5", Class = "pink", Img = "./images/Icon/UE5.png", Progress = 90 },
         new() { Name = "C#", Class = "green", Img = "./images/Icon/C2.png", Progress = 80 },
         new() { Name = "Blender", Class = "blue", Img = "./images/Icon/Blender.png", Progress = 70 },
-        new() { Name = "Python", Class = "purply", Img = "./images/Icon/python.png", Progress = 40 },
-        new() { Name = "Pt", Class = "orange", Img = "./images/Icon/pt.png", Progress = 20 },
-        new() { Name = "S&box", Class = "yellow", Img = "./images/Icon/sbox.png", Progress = 0 },
+        new() { Name = "Python", Class = "purply", Img = "./images/Icon/python.png", Progress = 30 },
+        new() { Name = "Pt", Class = "orange", Img = "./images/Icon/pt.png", Progress = 40 },
+        new() { Name = "Ds", Class = "yellow", Img = "./images/Icon/Ds.png", Progress = 35 },
     ];
 
 
